246d767112a5df5388582e6b669db9bb2bcfec69
KozerT <kozer.vizastaff@gmail.com>
2024-06-30 20:38:07 +0200

    feat: impelemnt a hook to subscribe for the new orders that are inserted

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -2,6 +2,10 @@ import { ActivityIndicator, FlatList, StyleSheet, Text } from "react-native";
 import Colors from "@/src/constants/Colors";
 import OrderListItem from "@/src/components/OrderListItem";
 import { useAdminOrderList } from "@/src/api/orders";
+import { useEffect } from "react";
+import { supabase } from "@/src/lib/supabase";
+import { useQueryClient } from "@tanstack/react-query";
+import { useInsertOrderSubscription } from "@/src/api/orders/subscriptions";
 
 export default function OrdersScreen() {
   const {
@@ -9,6 +13,8 @@ export default function OrdersScreen() {
     isLoading,
     error,
   } = useAdminOrderList({ archived: false });
+
+  useInsertOrderSubscription();
 
   if (isLoading) {
     return <ActivityIndicator />;
