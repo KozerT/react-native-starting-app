e3d106daece2fb1db2c1cef10100467a39fad579
KozerT <kozer.vizastaff@gmail.com>
2024-06-30 20:44:12 +0200

    feat: implement a useUpdateOrderSubscription hook

--- src/api/orders/subscriptions.ts
+++ src/api/orders/subscriptions.ts
@@ -1,6 +1,7 @@
 import { supabase } from "@/src/lib/supabase";
 import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
+import { useUpdateOrder } from "@/src/api/orders";
 
 export const useInsertOrderSubscription = () => {
   const queryClient = useQueryClient();
@@ -17,6 +18,31 @@ export const useInsertOrderSubscription = () => {
         }
       )
       .subscribe();
+    return () => {
+      orders.unsubscribe();
+    };
+  }, []);
+};
+
+export const useUpdateOrderSubscription = (id: number) => {
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    const orders = supabase
+      .channel("custom-filter-channel")
+      .on(
+        "postgres_changes",
+        {
+          event: "UPDATE",
+          schema: "public",
+          table: "orders",
+          filter: `id=eq.${id}`,
+        },
+        (payload) => {
+          queryClient.invalidateQueries({ queryKey: ["orders", id] });
+        }
+      )
+      .subscribe();
+
     return () => {
       orders.unsubscribe();
     };
