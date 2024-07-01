b5b5b51ab3b0240dceadffbdb214f990e89d9a16
KozerT <kozer.vizastaff@gmail.com>
2024-07-01 22:47:34 +0200

    fix: issue with the InvalidWorkerCreation caused by the declaration of the strype function in the other file

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -3,24 +3,32 @@
 // This enables autocomplete, go to definition, etc.
 
 // Setup type definitions for built-in Supabase Runtime APIs
+import "https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts"
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
-import { stripe } from '../utils/stripe.ts';
+import Stripe from 'https://esm.sh/stripe@16.1.0?target=deno&deno-std=0.132.0&no-check';
 
-console.log('Hello from Functions!');
+
+const stripe = Stripe(Deno.env.get('STRIPE_SECRET_KEY') ?? '', {
+  httpClient: Stripe.createFetchHttpClient(),
+});
+
+
+console.log("Hello from Functions!")
 
 serve(async (req) => {
   try {
     const { amount } = await req.json();
 
+    // Create a PaymentIntent so that the SDK can charge the logged in customer.
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: parseInt(amount),
-      currency: 'sek',
+      amount: amount,
+      currency: 'usd',
     });
+    
     const res = {
       publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
       paymentIntent: paymentIntent.client_secret,
     };
-    
     return new Response(JSON.stringify(res), {
       headers: { 'Content-Type': 'application/json' },
       status: 200,
@@ -32,6 +40,7 @@ serve(async (req) => {
     });
   }
 });
+
 /* To invoke locally:
 
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
@@ -40,6 +49,6 @@ serve(async (req) => {
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
-    --data '{"amount":"1120"}'
+    --data '{"amount":"1299"}'
 
 */
